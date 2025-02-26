56c6b28cf98630292b5b6e5345b80ebb1e460245
deepakann <deepakann77@gmail.com>
2025-02-27 00:58:11 +0530

    Updated the tags for Public Subnet

--- eks-install/modules/vpc/main.tf
+++ eks-install/modules/vpc/main.tf
@@ -27,6 +27,14 @@ resource "aws_subnet" "public" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                           = "${var.cluster_name}-public-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+    "kubernetes.io/role/elb"                       = "1"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
